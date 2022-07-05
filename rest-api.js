1644526cc1696eda82d7355c993eb2ee28fea32d
bruz1c0 <nazariompedroso@gmail.com>
2022-07-05 02:34:49 -0300

    Backend Aula 3 - Rest

---
+++ rest-api.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express()
+const router = express.Router();
+const bodyParser = require('body-parser')
+
+app.use('/', express.static(__dirname + "/static"));
+app.use(bodyParser.json())
+
+router.route("/")
+    .all((req, res, next) => {
+        console.log('Request from : ${req.originalUrl}')
+        console.log('Request type : {req.method}')
+        console.log('Request params : ${req.params}')
+        next();
+})
+
+    .get((req, res) => {
+        console.log(req.params)
+        res.send(JSON.stringify([]))
+        res.status(200)
+        res.end()
+})
+
+    .post((req, res) => {
+        console.log(req.body)
+        res.status(201)
+        res.end()
+})
+
+router
+    .param('id', (req, res, next, id) => {
+        console.log('Request from : ${req.params}')
+        console.log('Request type : ${req.method}')
+        console.log('Request id : ${id}')
+        nest()
+    })
+
+    .route("/:id")
+    .get((req, res) => {
+        console.log(req.params)
+        res.send(JSON.stringify({}))
+        res.status(200)
+        res.end()
+    })
+
+    .put((req, res) => {
+        console.log(req.body)
+        res.status(203)
+        res.end()
+    })
+
+    .delete((req, res) => {
+        res.status(203)
+        res.end()
+    });
+
+app.use('/posts', router);
+
+app.listen(4000, () => {
+    console.log("Server chorando se foi on http://localhost:4000")
+})
